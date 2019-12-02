85def5b8ebaa8541c9e2fc569c9c58352068a2b2
66112 <1031802507@qq.com>
2019-12-02 10:43:35 +0800

    可以交易多次

--- 买卖股票的最大利润.cpp
+++ 买卖股票的最大利润.cpp
@@ -1,4 +1,4 @@
-//https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
+https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
 int maxProfit(vector<int>& prices) {
 	int last = 0, ret = 0;
 	for (int i = 0; i < (int)prices.size() - 1; ++i) {
@@ -6,4 +6,14 @@ int maxProfit(vector<int>& prices) {
 		ret = max(profit, last);
 	}
 	return profit;
+}
+
+https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+int maxProfit(vector<int>& prices) {
+	int ret = 0;
+	for (int i = 1; i<prices.size(); i++){
+		if (prices[i] > prices[i - 1])
+			ret += prices[i] - prices[i - 1];
+	}
+	return ret;
 }
