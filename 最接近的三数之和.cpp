269cdaf2b73f563ee7728eea8d2a98f5e9bb2179
66112 <1031802507@qq.com>
2019-12-17 13:53:04 +0800

    双指针法

--- 最接近的三数之和.cpp
+++ 最接近的三数之和.cpp
@@ -21,3 +21,25 @@ int threeSumClosest(vector<int>& nums, int target) {
 	}
 	return ret;
 }
+
+//·½·¨¶ş
+int threeSumClosest(vector<int>& nums, int target) {
+	int ret = nums[0] + nums[1] + nums[2];
+	sort(nums.begin(), nums.end());
+	for (int i = 0; i<nums.size() - 2; i++){
+		int left = i + 1, right = nums.size() - 1;
+		while (left < right){
+			int tmp = nums[i] + nums[left] + nums[right];
+			if (abs(tmp - target) < abs(ret - target))
+				ret = tmp;
+			if (tmp > target)
+				right--;
+			else if (tmp < target)
+				left++;
+			else
+				return tmp;
+		}
+	}
+	return ret;
+}
+
